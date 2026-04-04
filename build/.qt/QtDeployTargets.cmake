e00552e0795415700c7818708bd4a8a799601e58
AndrzejKoprowski <251452@edu.p.lodz.pl>
2026-04-04 12:51:13 +0200

    v2

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Spektrometr_FILE C:/Users/andrz/OneDrive/Pulpit/Spektrometr/build/Spektrometr.exe)
+set(__QT_DEPLOY_TARGET_Spektrometr_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Spektrometr_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6SerialPortd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Cored.dll)
